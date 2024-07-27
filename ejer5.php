35ece3253e725b6c1340984299df8515f7c2df50
LauraFajar <lauracamilafajardocalderon@gmail.com>
2024-07-27 10:34:13 -0500

    Add files via upload

---
+++ ejer5.php
@@ -0,0 +1,16 @@
+<?php
+function FierroAlv (){
+    for($i=1; $i<=100; $i++){
+        if($i %3 === 0 && $i %5 === 0){
+            echo "PesoPluma";
+        }elseif ($i %3 === 0){
+            echo "Peso";
+        }elseif ($i %5 === 0){
+            echo "Pluma";
+        }else{
+            echo
+$i . " ";
+        }
+    }
+}
+
